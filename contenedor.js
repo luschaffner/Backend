e9afbd5a8c2abc603a08045fcbfd2923bd30defa
lucas schaffner <lucasschaffner@gmail.com>
2022-12-08 23:21:28 -0300

    API rest full

--- contenedor.js
+++ contenedor.js
@@ -1,94 +1,69 @@
-const fs = require("fs");
+const fs = require("fs")
 
 class Contenedor {
     constructor(fileName) {
-        this.newFile = fileName;
+        this.newFile = fileName
     }
-
 saveData = async (data) => {
     try {
-        await fs.promises.writeFile(this.newFile, JSON.stringify(data, null, 2));
+        await fs.promises.writeFile(this.newFile, JSON.stringify(data, null, 2))
     } catch (err) {
-        console.log("error escritura en archivo!", err);
+        console.log("error escritura en archivo!", err)
     }
-};
+}
 
 save = async (item) => {
     const productsArray = (await this.getAll()) || [];
     try {
         let id = 0;
-        productsArray.length === 0 ? (id = 1) : (id = productsArray[productsArray.length - 1].id + 1);
-        const newItem = { ...item, id: id };
-        productsArray.push(newItem);
-        await this.saveData(productsArray);
-        console.log(`producto ${item.title} ingresado ok!`);
-        return newItem.id;
-    } catch (err) {
-        console.log("error escritura en archivo!", err);
+        productsArray.length === 0 ? (id = 1) : (id = productsArray[productsArray.length - 1].id + 1)
+        const newItem = { ...item, id: id }
+        productsArray.push(newItem)
+        await this.saveData(productsArray)
+        console.log(`producto ${item.product} ingresado ok!`)
+        return newItem} catch (err) {
+            console.log("error escritura en archivo!", err)
     }
-};
+}
 
 getById = async (id) => {
     const productsArray = (await this.getAll()) || [];
     try {
-        const productById = productsArray.find((product) => product.id === id);
-        console.log("Producto encontrado: \n", productById || "No existe id");
-        return productById || null;
+        const productById = productsArray.find((product) => product.id === id)
+        return productById || null
     } catch (err) {
-        console.log("Error, ", err);
+        console.log("Error, ", err)
     }
-};
+}
 
 getAll = async () => {
     try {
-        const content = await fs.promises.readFile(this.newFile);
-        const contentArray = JSON.parse(content);
-        console.log("Productos: \n", contentArray);
-        return contentArray;
+        const content = await fs.promises.readFile(this.newFile)
+        const contentArray = JSON.parse(content)
+        return contentArray
     } catch (err) {
-        console.log("Archivo vacío");
-    }
-};
+        console.log("Archivo vacío")
+    }}
 
 deleteById = async (id) => {
     const productsArray = (await this.getAll()) || [];
     try {
-        const filteredProducts = productsArray.filter((product) => product.id !== id);
-        this.saveData(filteredProducts);
+        const filteredProducts = productsArray.filter((product) => product.id !== id)
+        this.saveData(filteredProducts)
     } catch (err) {
-        console.log("Error, ", err);
+        console.log("Error, ", err)
     }
-};
+}
 
 deleteAll = async () => {
     try {
         this.saveData([]);
         console.log("Productos eliminados!");
     } catch (err) {
-        console.log("Error, productos no eliminados!", err);
+        console.log("Error, productos no eliminados!", err)
     }
-};
+}
 }
 
-const item = {
-    title: "Caipiriña",
-    price: 650,
-    thumbnail:"https://pixabay.com/get/g760cf7cd61b812ca6ad1ff8fe028aa1d2d1e0dcf26808873c4f8ebf64a1f208403787f75b937155bd0f9b6c571d980b6446a6b4a83c525c43c713f3f0d158efc7de38220d34fd8172845a5d941d9dbf4_1920.jpg",
-};
-
-const item1 = {
-    title: "Gin tonic",
-    price: 780,
-    thumbnail:"https://pixabay.com/get/g11928134471bc5d6d146265e8be7dac97b4bcf1325cf1e494bb8c7f50c3197476aa933d606ea4d5193c7db2bc027b99c97ca6b26ebd8abea4459436a14ccd5b5d2d4f7a24776c608d2588026d1384dcf_1920.jpg",
-};
-
-const item2 = {
-    title: "Martini",
-    price: 700,
-    thumbnail:"https://pixabay.com/get/gd63aaced90250e462afc0922562a69f7424ef2597133f2c36617cb2114a38754739069ef7fe2771e82b1ce8a64a92d56b151cee492e5eea5c89f199bf461b8edc947fef6824f7ce9050b2004f7055581_1920.jpg",
-};
-
-const productos = new Contenedor("productos.txt");
-
-productos.save(item1)
+module.exports = Contenedor
 
--- server.js
+++ server.js
@@ -1,25 +1,53 @@
-const express = require('express')
-const Container = require('./contenedor')
-
-const app = express()
-
-const PORT = 8080
-
-const server = app.listen(PORT, () => {
-    console.log(`servidor http escuchando en puerto ${server.address().port}`)
+const server = app.listen(PORT, () =>{
+    console.log(`Servidor HTTP escuchando en puerto ${server.address().port}`)
 })
-
 server.on('error', error => console.log(`Error en servidor, ${error}`))
+
+const routeProducts = express.Router()
+
+app.use('/api/productos', routeProducts)
+app.use(express.static('/public'))
+routeProducts.use(express.urlencoded({ extended: true }))
+routeProducts.use(express.json())
 
 const products = new Container('products.txt')
 
-app.get('/productos', async (req, res) => {
-    const productsList = await products.getAll()
-    res.send(productsList)
+routeProducts.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html')
 })
 
-app.get('/productoRandom', async (req, res) => {
+// devuelve todos los productos
+routeProducts.get('/listProducts', async (req, res) => {
     const productsList = await products.getAll()
-    const randomProduct = Math.floor(Math.random() * productsList.length)
-    res.send(productsList[randomProduct])
+    res.json(productsList)
+})
+
+routeProducts.get('/:id', async (req, res) =>{
+    const productById = await products.getById(parseInt(req.params.id))
+    productById === null ? res.json({ Error:  'Producto no encontrado' }) : res.json(productById)
+})
+
+routeProducts.post('/addProduct', async (req, res) =>{
+    const savedProduct = await products.save(req.body)
+    res.json(savedProduct)
+})
+
+routeProducts.put('/:id', async (req, res) =>{
+    const updateInfo = req.body
+    const productsList = await products.getAll()
+    regToUpdate = productsList.findIndex(product => product.id === parseInt(req.params.id))
+    if (regToUpdate === -1) {
+        return res.send({ Error:  'Producto no encontrado' })
+    }
+    productsList[regToUpdate] = { ...updateInfo, id: parseInt(req.params.id) }
+    await products.saveData(productsList)
+    res.json({ ...updateInfo, id: parseInt(req.params.id) })
+})
+
+routeProducts.delete('/:id', async (req, res) =>{
+    const deletedId = await products.getById(parseInt(req.params.id))
+    await products.deleteById(parseInt(req.params.id))
+    deletedId === null
+        ? res.json( {'Producto con ID': `${parseInt(req.params.id)} no encontrado`} )
+        : res.json( {'Producto eliminado': deletedId} )
 })
